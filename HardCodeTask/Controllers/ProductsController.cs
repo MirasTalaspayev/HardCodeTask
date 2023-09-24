1127a4a43d547f6b36c44a0710e318a5c366279a
unknown <tmk01.12.00@gmail.com>
2023-09-24 21:29:24 +0600

    added controllers

---
+++ HardCodeTask/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using HardCodeTask.Application.DTOs.Categories;
+using HardCodeTask.Application.DTOs.Products;
+using HardCodeTask.Application.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HardCodeTask.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+    [HttpPost("add")]
+    public bool AddCategory([FromBody] ProductDTO category)
+    {
+        return _productService.Add(category);
+    }
+    [HttpGet("{id}")]
+    public ProductDTO GetById(int id) 
+    {
+        return _productService.GetById(id);
+    }
+    [HttpGet]
+    public ICollection<ProductDTO> GetAll()
+    {
+        return _productService.GetAll();
+    }
+}
